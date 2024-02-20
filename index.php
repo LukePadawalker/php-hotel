f71e0f261d5f95e442544b593bfaaa8d184e3f71
LukePadawalker <padurariu.luca@gmail.com>
2024-02-20 17:12:44 +0200

    created layout

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Hotel</title>
+</head>
+<body>
+    <div class="row d-flex">
+
+        <div class="col-3 d-flex justify-content-center">cioa</div>
+        <div class="col-2 d-flex justify-content-center">cioaca</div>
+        <div class="col-2 d-flex justify-content-center">caioca</div>
+        <div class="col-2 d-flex justify-content-center">icoaca</div>
+        <div class="col-3 d-flex justify-content-center">acioa</div>
+    </div>
+    
+
+</body>
+</html>
